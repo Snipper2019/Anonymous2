67bf8213beb86270634bf84f861dae967e4237ee
Tendekayi Gwara <49101@iieconnect.co.za>
2019-09-13 11:49:02 +0200

    Initial commit

---
+++ Anonymous2/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Anonymous2
+{
+
+    public delegate string GreetingDel(string name);
+    class Program
+    {
+
+
+        static void Main(string[] args)
+        {
+            GreetingDel gt = delegate(string name)
+            {
+                return "hello" + name + "how are you";
+            };
+
+            string str = gt.Invoke("Tendeg");
+            Console.WriteLine(str);
+            Console.ReadLine();
+
+        }
+    }
+}
